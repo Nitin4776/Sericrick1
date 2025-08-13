0676a57165a19edfe9ecff8364eeabd9fa0c555f
Nitin chaudhary <nchaudhary836@gmail.com>
2025-08-13 08:33:58 +0000

    ok I want to convert this app to android app using capicator let me know

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sericrick.app',
+  appName: 'SeriCrick',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
